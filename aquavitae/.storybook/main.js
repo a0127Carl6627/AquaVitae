9511c810d05130e9165517f2b0153e7f9e013b4f
Carlos Olivarez <carmanolmon@gmail.com>
2026-04-15 13:40:40 -0600

    Avatsr/profilepicture

--- aquavitae/.storybook/main.js
+++ aquavitae/.storybook/main.js
@@ -1,20 +1,20 @@
-
-
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
 const config = {
-  "stories": [
+  stories: [
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/preset-create-react-app",
     "@storybook/addon-a11y",
     "@storybook/addon-docs",
     "@storybook/addon-onboarding"
   ],
-  "framework": "@storybook/react-webpack5",
-  "staticDirs": [
-    "..\\public"
-  ]
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  staticDirs: ["..\\public"],
 };
+
 export default config;
