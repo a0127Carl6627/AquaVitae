7b17328bbbe9a6af4dc72a8325785e1635c463de
Carlos Olivarez <carmanolmon@gmail.com>
2026-04-17 17:39:09 -0600

    PlantListCard

---
+++ aquavitae/src/stories/PlantListCard.stories.js
@@ -0,0 +1,10 @@
+import PlantListCard from './PlantListCard';
+
+const meta = {
+  title: 'SCRUM-36-37/PlantListCard',
+  component: PlantListCard,
+};
+
+export default meta;
+
+export const Default = {};
