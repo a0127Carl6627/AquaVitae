bd9e409464aadced5cf6b954adbec5bc6ffacfd5
Carlos Olivarez <carmanolmon@gmail.com>
2026-04-17 17:47:40 -0600

    MapAlertPopup

---
+++ aquavitae/src/stories/MapAlertPopup.stories.js
@@ -0,0 +1,16 @@
+import MapAlertPopup from './MapAlertPopup';
+
+const meta = {
+  title: 'SCRUM-38/MapAlertPopup',
+  component: MapAlertPopup,
+};
+
+export default meta;
+
+export const Default = {
+  args: {
+    title: 'Alerta Monterrey 1',
+    description:
+      'Disponibilidad hídrica disminuyó 15% en los últimos 7 días. Requiere acción inmediata.',
+  },
+};
