2011e43ec0b789062cba0a38d4f6a3799eb13676
Carlos Olivarez <carmanolmon@gmail.com>
2026-04-17 17:10:41 -0600

    RegionDropDown

---
+++ aquavitae/src/stories/RegionDropdown.stories.js
@@ -0,0 +1,10 @@
+import RegionDropdown from './RegionDropdown';
+
+const meta = {
+  title: 'SCRUM-34/RegionDropdown',
+  component: RegionDropdown,
+};
+
+export default meta;
+
+export const Default = {};
